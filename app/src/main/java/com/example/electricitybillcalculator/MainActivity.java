f0e0aebe6228fd8e8a474ade3f33415ea8ccd652
Hilmeez <nuruddinalhilmi6.com>
2024-11-30 06:30:00 +0800

    Latest Updated

--- app/src/main/java/com/example/electricitybillcalculator/MainActivity.java
+++ app/src/main/java/com/example/electricitybillcalculator/MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu); // Inflate the menu XML file
+        inflater.inflate(R.menu.menu, menu);
         return true;
     }
 
